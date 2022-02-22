34647bb658372075cb99a706db71c1872f4d4420
Daisyyyyy0 <soniahsiao16@gmail.com>
2022-02-23 02:07:45 +0800

    sass not found

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import PropsTest from './components/PropsTest.vue'
+// import PropsTest from '@/components/PropsTest.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components:{
+    PropsTest,
+  }
+}
+</script>
+  
+<template>
+  <PropsTest/>
+</template>
+
+<style >
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PropsTest.vue
@@ -0,0 +1,18 @@
+
+<script>
+export default {
+    
+}
+</script>
+
+<template >
+    <h1>Daisy</h1>
+</template>
+
+<style lang="scss">
+    h1{
+        color:aqua;
+        font-size: 50px;
+        
+    }
+</style>
